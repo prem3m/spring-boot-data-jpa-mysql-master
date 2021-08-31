861e4601ff8a18609b99ff508de7efe5b7a96a99
prem3m <prem3j@gmail.com>
2021-08-31 19:09:41 +0530

    commit 2

---
+++ src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
@@ -0,0 +1,15 @@
+package com.bezkoder.spring.datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class SpringBootDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpaApplication.class, args);
+	}
+
+}
